fb82a2076128549007ebec48f7de8c6f50494927
Ibrahim <ibra.653@gmail.com>
2025-10-23 10:31:17 +0500

    fixed ipv6 null error

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "vpc" {
 
 # SUBNET
 resource "aws_subnet" "public" {
-  count                                           = var.vpc_settings.enable_aws_ipv6_cidr_block != null && var.vpc_settings.enable_aws_ipv6_cidr_block.ipv6_native ? var.vpc_settings.enable_aws_ipv6_cidr_block.public_cidr_count_prefix64 : length(var.vpc_settings.public_subnet_cidr_blocks)
+  count                                           = try(var.vpc_settings.enable_aws_ipv6_cidr_block.ipv6_native, false) ? try(var.vpc_settings.enable_aws_ipv6_cidr_block.public_cidr_count_prefix64, 0) : length(var.vpc_settings.public_subnet_cidr_blocks)
   vpc_id                                          = aws_vpc.vpc.id
   cidr_block                                      = try(var.vpc_settings.enable_aws_ipv6_cidr_block.ipv6_native, false) || length(var.vpc_settings.public_subnet_cidr_blocks) == 0 ? null : var.vpc_settings.public_subnet_cidr_blocks[count.index]
   ipv6_cidr_block                                 = try(var.vpc_settings.enable_aws_ipv6_cidr_block.public_cidr_count_prefix64 != 0 ? cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 8, count.index) : null)
